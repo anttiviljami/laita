929ec04f41ba57114f5e2ab6cb9eb807604b7091
Viljami Kuosmanen <viljami@avoinsorsa.fi>
2019-07-02 20:16:39 +0300

    Use separate providers for each stage

--- terraform/templates/aws-s3-cloudfront.ejs.tf
+++ terraform/templates/aws-s3-cloudfront.ejs.tf
@@ -1,8 +1,14 @@
-provider "aws" { region = "<%= opts.region %>" }
+provider "aws" {
+  region = "<%= opts.region %>"
+  alias = "<%= opts.stage %>"
+}
 
 module "s3_website_<%= opts.stage %>" {
   source = "./static-s3-website"
   bucket_name = "<%= opts.bucketName %>"
+  providers = {
+    aws = aws.<%= opts.stage %>
+  }
 }
 output "s3_endpoint_<%= opts.stage %>" {
   value = "http://${module.s3_website_default.website_endpoint}"
@@ -12,6 +18,9 @@ output "s3_endpoint_<%= opts.stage %>" {
 module "cloudfront_distribution_<%= opts.stage %>" {
   source = "./cloudfront-distribution"
   website_endpoint = "${module.s3_website_default.website_endpoint}"
+  providers = {
+    aws = aws.<%= opts.stage %>
+  }
 }
 output "cloudfront_endpoint_<%= opts.stage %>" {
   value = "${module.cloudfront_distribution_<%= opts.stage %>.endpoint_url}"
